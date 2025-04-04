42dfd1ce88bae676d6247f2bb41b7a850f5c97a7
Do Hoang Bao <dohoangbao420@gmail.com>
2025-04-04 09:18:05 +0700

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'login-app',
+  webDir: 'dist'
+};
+
+export default config;
